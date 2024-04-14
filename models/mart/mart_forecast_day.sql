3fb8fc94957c63dc294b99a8e96b26c3f5f012d2
Alex Schirokow <aschirokow@gmx.de>
2024-04-14 23:28:22 +0200

    day mart + no moonrise + pressure_mb

--- models/mart/mart_forecast_day.sql
+++ models/mart/mart_forecast_day.sql
@@ -3,18 +3,22 @@ WITH joining_day_location AS (
         LEFT JOIN {{ref('staging_location')}}
         USING(city,region,country)
 ),
--- pressure_mb_daily AS (
---         SELECT date, city, region, country, ROUND(AVG(pressure_mb),0) AS pressure_mb_avg 
---         FROM prep_forecast_hour 
---         GROUP BY (date, city, region, country)
--- ),
+pressure_mb_daily AS (
+        SELECT date, city, region, country, ROUND(AVG(pressure_mb),0) AS pressure_mb_avg 
+        FROM prep_forecast_hour 
+        GROUP BY (date, city, region, country)
+),
 adding_features AS (
         SELECT 
-            *,
-            CONCAT('&nbsp;&nbsp;&nbsp;&nbsp;![weather_icon](',condition_icon,'?width=35)') AS condition_icon_md
+            *
+            ,CONCAT('&nbsp;&nbsp;&nbsp;&nbsp;![weather_icon](',condition_icon,'?width=35)') AS condition_icon_md
+            ,CASE WHEN moonrise = 'No moonrise' THEN null ELSE moonrise END moonrise_n
+            ,CASE WHEN moonset = 'No moonset' THEN null ELSE moonset END moonset_n
+            ,CASE WHEN sunrise = 'No sunrise' THEN null ELSE sunrise END sunrise_n
+            ,CASE WHEN sunset = 'No sunset' THEN null ELSE sunset END sunset_n
         FROM joining_day_location
-        -- LEFT JOIN pressure_mb_daily
-        -- USING(date, city,region,country)
+        LEFT JOIN pressure_mb_daily
+        USING(date, city,region,country)
 ),
 filtering_ordering_features AS (
         SELECT 
@@ -37,7 +41,7 @@ filtering_ordering_features AS (
             ,total_precip_mm
             ,total_snow_cm
             ,avg_humidity
-            -- ,pressure_mb_avg
+            ,pressure_mb_avg
             ,daily_will_it_rain
             ,daily_chance_of_rain
             ,daily_will_it_snow
@@ -49,10 +53,10 @@ filtering_ordering_features AS (
             ,max_wind_kph
             ,avg_vis_km
             ,uv
-            ,sunrise
-            ,sunset
-            ,moonrise
-            ,moonset
+            ,sunrise_n
+            ,sunset_n
+            ,moonrise_n
+            ,moonset_n
             ,moon_phase
             ,moon_illumination
         FROM adding_features
